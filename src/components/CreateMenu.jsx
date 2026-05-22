dd42de85d1ff3ccf5d108fb21b655401e8ab16c3
lucasdevsilva26 <lucasferreirasilvaadry@gmail.com>
2026-05-22 10:05:10 -0300

    update item list

--- src/components/CreateMenu.jsx
+++ src/components/CreateMenu.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import "./createMenu.css";
 
 function CreateMenu() {
-const [image, setImage] = useState("")
+  const [items, changeItems] = useState([])
+
+  const [image, setImage] = useState("")
+  const [name, changeName] = useState()
+  const [price, changeprice] = useState()
+  const [amount, changeAmount] = useState()
 
   function handleImage(event) {
     const file = event.target.files[0];
@@ -14,27 +19,66 @@ const [image, setImage] = useState("")
     setImage(imageUrl)
   }
 
+function createItem(name, price, amount) {
+  if (name && price && amount) {
+    if (name.trim().length == 0 && price.trim().length == 0 && amount.trim().length == 0) { return }
+
+    const newItem = {
+      id:crypto.randomUUID(),
+      image: 1,
+      name: name,
+      price: price,
+      amount: amount,
+    }
+
+    items.filter((item) => {if (item.name == newItem.name) { return }})
+
+    changeItems([...items, newItem])
+  }
+}
+
   return (
     <>
       <section id="createMenu">
-        <input type="file" accept="image/*" onChange={handleImage} style={{backgroundImage:`url(${image})`}}/>
+        <section>
+          <input type="file" accept="image/*" onChange={handleImage} style={{backgroundImage:`url(${image})`}}/>
 
-        <form>
-          <div>
-            <label htmlFor="itemName">Nome do Produto</label>
-            <input type="text" id="itemName"/>
-          </div>
+          <form>
+            <div>
+              <label htmlFor="itemName">Nome do Produto</label>
+              <input type="text" id="itemName" onChange={(e) => changeName(e.target.value)}/>
+            </div>
 
-          <div>
-            <label htmlFor="itemPrice">Preço do Produto</label>
-            <input type="number" id="itemPrice" />
-          </div>
+            <div>
+              <label htmlFor="itemPrice">Preço do Produto</label>
+              <input type="number" id="itemPrice" onChange={(e) => changeprice(e.target.value)}/>
+            </div>
 
-          <div>
-            <label htmlFor="itemAmount">Quantidade</label>
-            <input type="number" id="itemAmount" />
-          </div>
-        </form>
+            <div>
+              <label htmlFor="itemAmount">Quantidade</label>
+              <input type="number" id="itemAmount" onChange={(e) => changeAmount(e.target.value)}/>
+            </div>
+          </form>
+        </section>
+
+        <button onClick={() => createItem(name, price, amount)}>Criar</button>
+      </section>
+
+      <section id="itemList">
+        {items.map((item) => {
+          return ( 
+            <article key={item.id} className="itemCard"> 
+
+              <img/>
+
+              <h2> { item.name } </h2>
+
+              <span> { item.price } </span>
+
+              <span> { item.amount } </span>
+            </article>
+          )
+        })}
       </section>
     </>
   )
