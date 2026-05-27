141b665c9c9d0f5a44ae429928fe912b622eb2a4
lucasdevsilva26 <lucasferreirasilvaadry@gmail.com>
2026-05-26 22:52:27 -0300

    update desing responsive

--- src/components/IconList.jsx
+++ src/components/IconList.jsx
@@ -1,6 +1,11 @@
 import "./iconList.css";
 
 function IconList({ icons, iconChange, color, iconsMenu, changeIconsMenu }) {
+  function setIcon(icon) {
+    iconChange(icon);
+    changeIconsMenu(false);
+  }
+  
   return (
     <>
       <section
@@ -10,22 +15,26 @@ function IconList({ icons, iconChange, color, iconsMenu, changeIconsMenu }) {
       >
         <div>
           {icons.map((icon) => {
-          return (
-            <button
-              key={icon.id}
-              className="responButton"
-              onClick={() => iconChange(icon.icon)}
-            >
-              <i
-                className={`fas ${icon.icon} textShadow`}
-                style={{ color: `${color}` }}
-              ></i>
-            </button>
-          );
-        })}
+            return (
+              <button
+                key={icon.id}
+                className="responButton"
+                onClick={() => setIcon(icon.icon)}
+              >
+                <i
+                  className={`fas ${icon.icon} textShadow`}
+                  style={{ color: `${color}` }}
+                ></i>
+              </button>
+            );
+          })}
         </div>
 
-        <button id="iconListExitButton" className="responButton coloredButton" onClick={() => changeIconsMenu(false)}>
+        <button
+          id="iconListExitButton"
+          className="responButton coloredButton"
+          onClick={() => changeIconsMenu(false)}
+        >
           <i className="fas fa-xmark"></i>
         </button>
       </section>
