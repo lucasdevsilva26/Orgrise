c544d6f22567c66db92855a873ca8acce4d877a0
lucasdevsilva26 <lucasferreirasilvaadry@gmail.com>
2026-06-09 11:55:25 -0300

    update card and card anims

--- src/components/header/LoggedSideBar.jsx
+++ src/components/header/LoggedSideBar.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import { Global } from "../../main";
 
-function LoggedSideBar({presets}) {
-    const navigate = useNavigate()
+function LoggedSideBar({ presets }) {
+  const navigate = useNavigate();
 
-    const [sideMenus, setSideMenus] = useState(false);
-    const menus = [
+  const [sideMenus, setSideMenus] = useState(false);
+  const menus = [
+    { path: "Landing", name: "Tutorial", icon: "fas fa-brain" },
     { path: "Charts", name: "Gráficos", icon: "fas fa-chart-column" },
     { path: "Inventory", name: "Estoque", icon: "fas fa-boxes-stacked" },
     { path: "Orders", name: "Pedidos", icon: "fas fa-truck-fast" },
@@ -25,39 +26,39 @@ function LoggedSideBar({presets}) {
     },
   ];
 
-    return (
-        <aside
-                className={`flex flex-col items-center gap-4 w-50 px-6 h-full py-18 ${Global.colors.background} border-r-2 ${Global.colors.border} fixed top-0 right-full text-base! ${sideMenus ? "translate-x-full" : "translate-x-0"} duration-500 ${localStorage.getItem('logged') === 'true' ? "" : "hidden!"} z-50`}
-              >
-                {menus.map((menuUnit) => {
-                  return (
-                    <button
-                      key={menuUnit.path}
-                      className={presets.button + presets.scaleChange}
-                      onClick={() =>
-                        navigate(
-                          "/" +
-                            menuUnit.path.charAt(0).toLowerCase() +
-                            menuUnit.path.slice(1),
-                        )
-                      }
-                    >
-                      <i className={menuUnit.icon}></i>
-                      {menuUnit.name}
-                    </button>
-                  );
-                })}
-        
-                <button
-                  className={
-                    `flex items-center justify-center absolute top-18 left-full ml-2 w-10 h-10 rounded-4xl ${Global.colors.buttonBg} text-white font-bold text-3xl ${sideMenus ? "rotate-180" : "rotate-0"} duration-1000 opacity-75` +
-                    presets.scaleChange
-                  }
-                  onClick={() => setSideMenus(!sideMenus)}
-                >
-                  <i className={`fas fa-angle-right`}></i>
-                </button>
-              </aside>
-    )
+  return (
+    <aside
+      className={`flex flex-col items-center gap-4 w-50 px-6 h-full py-18 ${Global.colors.background} border-r-2 ${Global.colors.border} fixed top-0 right-full text-base! ${sideMenus ? "translate-x-full" : "translate-x-0"} duration-500 ${localStorage.getItem("logged") === "true" ? "" : "hidden!"} z-50`}
+    >
+      {menus.map((menuUnit) => {
+        return (
+          <button
+            key={menuUnit.path}
+            className={presets.button + presets.scaleChange}
+            onClick={() =>
+              navigate(
+                "/" +
+                  menuUnit.path.charAt(0).toLowerCase() +
+                  menuUnit.path.slice(1),
+              )
+            }
+          >
+            <i className={menuUnit.icon}></i>
+            {menuUnit.name}
+          </button>
+        );
+      })}
+
+      <button
+        className={
+          `flex items-center justify-center absolute top-18 left-full ml-2 w-10 h-10 rounded-4xl ${Global.colors.buttonBg} text-white font-bold text-3xl ${sideMenus ? "rotate-180" : "rotate-0"} duration-1000 opacity-75` +
+          presets.scaleChange
+        }
+        onClick={() => setSideMenus(!sideMenus)}
+      >
+        <i className={`fas fa-angle-right`}></i>
+      </button>
+    </aside>
+  );
 }
-export default LoggedSideBar
+export default LoggedSideBar;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -12,14 +12,40 @@ function Landing() {
       <Header colors={Global.colors}></Header>
       <Body colors={Global.colors} direction={"flex-row"}>
         <section
-          className={` flex flex-col items-center w-full py-10 relative `}
+          className={` flex flex-col items-center gap-10 w-full py-10 relative `}
         >
-          <h1 className={` text-4xl font-bold `}>Como começar?</h1>
-          <p></p>
+          <h1 className={` text-4xl font-bold ${Global.colors.title} `}>
+            Como começar?
+          </h1>
+
+          <div
+            className={`flex flex-col place-items-center w-full py-10 ${Global.colors.frontground1}`}
+          >
+            <div className="flex flex-col place-items-center gap-10 max-w-1/2 ">
+              <h2 className={`text-5xl font-bold ${Global.colors.title} `}>
+                Como usar o menu lateral:
+              </h2>
+
+              <p className={`text-2xl text-justify `}>
+                {" "}
+                O menu localizado à esquerda reúne todas as principais
+                ferramentas do sistema. Basta clicar em uma opção para acessar
+                sua funcionalidade.
+              </p>
+            </div>
+          </div>
+
+          <section>
+            <div className={`flex flex-col place-items-center gap-4 w-3/10 p-5 rounded-2xl ${Global.colors.frontground0} border-2 ${Global.colors.border} flip3D swordGlow movimentAnim`}>
+              <i className={`fas fa-brain text-5xl ${Global.colors.title}`}></i>
+              <h1 className={`text-3xl font-bold ${Global.colors.title}`}>Início</h1>
+              <p className="text-xl text-justify">Estoque Cadastre, edite e acompanhe todos os produtos disponíveis em seu estoque.</p>
+            </div>
+          </section>
 
           <span className=" flex place-items-center gap-2 absolute text-yellow-500 font-bold left-1/25 animate-bounce">
-            <i className="fas fa-hand-point-left text-yellow-500 text-2xl"></i>Clique aqui
-            para a abrir o menu!
+            <i className="fas fa-hand-point-left text-yellow-500 text-2xl"></i>
+            Clique aqui para a abrir o menu!
           </span>
         </section>
       </Body>
